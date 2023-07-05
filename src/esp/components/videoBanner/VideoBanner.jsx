342a434d9b661ef8ead76595e03421097ac699f5
javiertruquin <javiertruquin@gmail.com>
2023-07-05 18:33:01 -0300

    cambio de videos en home

--- src/esp/components/videoBanner/VideoBanner.jsx
+++ src/esp/components/videoBanner/VideoBanner.jsx
@@ -1,27 +1,20 @@
 import "./videoBanner.css";
 import ReactPlayer from "react-player";
+import video1 from "../../assets/video/escritoriolargo.mp4"
+import video2 from "../../assets/video/videodkz.mp4"
+import video3 from "../../assets/video/celular.mp4"
+
 
 export default function VideoBanner() {
-    const youtubeConfig = {
-        youtube: {
-            playerVars: {
-                controls: 0, // Oculta los controles del reproductor
-                modestbranding: 1, // Oculta el logo de YouTube
-                disablekb: 1, // Desactiva el uso del teclado para pausar el video
-                rel: 0, // Desactiva los videos relacionados al final del video
-                showinfo: 0, // Oculta el título del video
-            },
-        },
-    };
+    
     return (
         <div className="pb-5">
             <div className="tamaño-video">
                 {/* PANTALLA GRANDE */}
                 <ReactPlayer
                     className="d-none d-xl-block"
-                    url="https://youtu.be/oX28ph8JiYU"
+                    url={video1}
                     muted
-                    config={youtubeConfig}
                     loop
                     playing
                     width="100%"
@@ -30,9 +23,8 @@ export default function VideoBanner() {
                 {/* ESCRITORIO */}
                 <ReactPlayer
                     className="d-none d-md-block d-xl-none"
-                    url="https://www.youtube.com/watch?v=Jdux5Es9gBM&t=1s"
+                    url={video2}
                     muted
-                    config={youtubeConfig}
                     loop
                     playing
                     width="100%"
@@ -41,9 +33,8 @@ export default function VideoBanner() {
                 {/* CELULAR */}
                 <ReactPlayer
                     className="d-block d-sm-none"
-                    url="https://youtu.be/XAjzAefz0pY"
+                    url={video3}
                     muted
-                    config={youtubeConfig}
                     loop
                     playing
                     width="100%"
