75f9a0d565d5baf0995a50a23f8996a2d336d985
javiertruquin <javiertruquin@gmail.com>
2023-07-06 19:32:13 -0300

    nueva forma de ver el video

--- src/esp/components/videoBanner/VideoBanner.jsx
+++ src/esp/components/videoBanner/VideoBanner.jsx
@@ -1,41 +1,25 @@
 import "./videoBanner.css";
-import ReactPlayer from "react-player";
 
 export default function VideoBanner() {
-    
     return (
         <div className="pb-5">
-            <div className="tamaño-video">
-                {/* PANTALLA GRANDE */}
-                <ReactPlayer
-                    className="d-none d-xl-block"
-                    url="https://res.cloudinary.com/dlzcbrqax/video/upload/v1688596266/VIDEOS%20BANNER/escritoriolargo_ynd1au.mp4"
-                    muted
-                    loop
-                    playing
-                    width="100%"
-                    height="100%"
-                ></ReactPlayer>
+            <div className="d-flex justify-content-center">
                 {/* ESCRITORIO */}
-                <ReactPlayer
-                    className="d-none d-md-block d-xl-none"
-                    url="https://res.cloudinary.com/dlzcbrqax/video/upload/v1688596296/VIDEOS%20BANNER/videodkz_gtb2fq.mp4"
-                    muted
+                <video
+                    className="video d-none d-sm-block"
+                    src="https://res.cloudinary.com/dlzcbrqax/video/upload/v1688596296/VIDEOS%20BANNER/videodkz_gtb2fq.mp4"
+                    autoPlay
                     loop
-                    playing
-                    width="100%"
-                    height="100%"
-                ></ReactPlayer>
+                    muted
+                ></video>
                 {/* CELULAR */}
-                <ReactPlayer
-                    className="d-block d-sm-none"
-                    url="https://res.cloudinary.com/dlzcbrqax/video/upload/v1688596266/VIDEOS%20BANNER/celular_o9fpoz.mp4"
-                    muted
+                <video
+                    className="video d-block d-sm-none"
+                    src="https://res.cloudinary.com/dlzcbrqax/video/upload/v1688596266/VIDEOS%20BANNER/celular_o9fpoz.mp4"
+                    autoPlay
                     loop
-                    playing
-                    width="100%"
-                    height="100%"
-                ></ReactPlayer>
+                    muted
+                ></video>
             </div>
         </div>
     );
