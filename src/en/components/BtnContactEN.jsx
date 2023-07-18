83ce21329fd50788570aa4281863b74343d00445
¨mago¨ <martingramajo08@gmail.com>
2023-07-18 16:55:21 -0300

    agrego btncontacten

---
+++ src/en/components/BtnContactEN.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default function BtnContact() {
+  return (
+    <div>
+      <Button as={Link} to="/contactanos" className="boton-contactanos">
+        <h4 className="peso-bold ">Contact</h4>
+      </Button>
+    </div>
+  );
+}
