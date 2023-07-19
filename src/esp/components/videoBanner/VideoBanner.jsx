6be4d59e79a5fc4e6519f0e145c554ea93875531
¨mago¨ <martingramajo08@gmail.com>
2023-07-19 14:11:31 -0300

    agrego codigo postal en footer

--- src/esp/components/videoBanner/VideoBanner.jsx
+++ src/esp/components/videoBanner/VideoBanner.jsx
@@ -1,27 +1,27 @@
 import "./videoBanner.css";
 
 export default function VideoBanner() {
-    return (
-        <div className="pb-5">
-            <div className="d-flex justify-content-center">
-                {/* ESCRITORIO */}
-                <video
-                    className="videoHome tamaño-video d-none d-sm-block"
-                    src="https://res.cloudinary.com/dlzcbrqax/video/upload/v1688596296/VIDEOS%20BANNER/videodkz_gtb2fq.mp4"
-                    autoPlay
-                    loop
-                    muted
-                ></video>
-                {/* CELULAR */}
-                <video
-                    className="videoHome d-block d-sm-none"
-                    src="https://res.cloudinary.com/dlzcbrqax/video/upload/v1689023000/VIDEOS%20BANNER/MobileCuadrado_ec0a4j.mp4"
-                    autoPlay
-                    loop
-                    muted
-                    playsinline
-                ></video>
-            </div>
-        </div>
-    );
+  return (
+    <div className="pb-5">
+      <div className="d-flex justify-content-center">
+        {/* ESCRITORIO */}
+        <video
+          className="videoHome tamaño-video d-none d-sm-block"
+          src="https://res.cloudinary.com/dlzcbrqax/video/upload/v1688596296/VIDEOS%20BANNER/videodkz_gtb2fq.mp4"
+          autoPlay
+          loop
+          muted
+        ></video>
+        {/* CELULAR */}
+        <video
+          className="videoHome d-block d-sm-none"
+          src="https://res.cloudinary.com/dlzcbrqax/video/upload/v1689023000/VIDEOS%20BANNER/MobileCuadrado_ec0a4j.mp4"
+          autoPlay
+          loop
+          muted
+          playsinline
+        ></video>
+      </div>
+    </div>
+  );
 }
