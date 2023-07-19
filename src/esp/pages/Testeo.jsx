848c95d1618ae3670c4be243ab981925030ec3e7
javiertruquin <javiertruquin@gmail.com>
2023-07-19 15:28:04 -0300

    nuevo intento video 30seg

--- src/esp/pages/Testeo.jsx
+++ src/esp/pages/Testeo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactPlayer from "react-player";
-import videoLocal from "../assets/video/originalnoenvenenado.mp4";
+import videoLocal from "../assets/video/originalsinveneno30.mp4";
 
 export default function Testeo() {
   return (
