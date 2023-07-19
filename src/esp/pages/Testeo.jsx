76c855b06fee3a3a96a117da73e9d6f29c98b1c0
¨mago¨ <martingramajo08@gmail.com>
2023-07-19 15:52:53 -0300

    limpio testeo

--- src/esp/pages/Testeo.jsx
+++ src/esp/pages/Testeo.jsx
@@ -19,55 +19,13 @@ export default function Testeo() {
             {/* CELULAR */}
             <video
               className="videoHome d-block d-sm-none"
-<<<<<<< HEAD
               src="https://youtu.be/oX28ph8JiYU"
-=======
-              src="https://res.cloudinary.com/dlzcbrqax/video/upload/v1689791260/VIDEOS%20BANNER/originalsinveneno30_bwx2fg.mp4"
->>>>>>> c66ed4b8e2d7323be5fa748e6a75070479a4c47f
               autoPlay
               loop
               muted
-              playsinline
             ></video>
           </div>
         </div>
-<<<<<<< HEAD
-=======
-        <div>
-          <div className="d-flex justify-content-center">
-            <ReactPlayer
-              playing
-              loop
-              muted
-              playsInline={true}
-              url="https://res.cloudinary.com/dlzcbrqax/video/upload/v1689791260/VIDEOS%20BANNER/originalsinveneno30_bwx2fg.mp4"
-            />
-          </div>
-        </div>
-        {/* local */}
-        <div>
-          <video
-            className="videoHome d-block d-sm-none"
-            src={videoLocal}
-            autoPlay
-            loop
-            muted
-            playsInline
-            controls={true}
-          ></video>
-        </div>
-        {/* react-player + local */}
-        <div className="d-flex justify-content-center">
-          <ReactPlayer
-            controls
-            playing
-            loop
-            muted
-            playsInline
-            url={videoLocal}
-          />
-        </div>
->>>>>>> c66ed4b8e2d7323be5fa748e6a75070479a4c47f
       </div>
     </div>
   );
