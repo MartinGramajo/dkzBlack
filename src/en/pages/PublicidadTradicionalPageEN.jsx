d8b5994c44422aacba1a79e6e01daff5ed7350a6
¨mago¨ <martingramajo08@gmail.com>
2023-08-30 17:37:20 -0300

    agrego nav y footer en las pages q faltaban

--- src/en/pages/PublicidadTradicionalPageEN.jsx
+++ src/en/pages/PublicidadTradicionalPageEN.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import PublicidadEN from "../components/publicidadTradicionalEN/PublicidadEN";
+import { NavbarReactV2EN } from "../components/navbarReactV2EN/NavbarReactV2EN";
+import { FooterReactEN } from "../components/footerReactEN/FooterReactEN";
 
 const PublicidadTradicionalPageEN = () => {
   return (
     <div>
+      <NavbarReactV2EN />
       <PublicidadEN />
+      <FooterReactEN />
     </div>
   );
 };
--- src/en/pages/SocialMediaPageEN.jsx
+++ src/en/pages/SocialMediaPageEN.jsx
@@ -1,9 +1,13 @@
+import { FooterReactEN } from "../components/footerReactEN/FooterReactEN";
+import { NavbarReactV2EN } from "../components/navbarReactV2EN/NavbarReactV2EN";
 import SocialEn from "../components/socialMediaEN/SocialEN";
 
 const SocialMediaPageEN = () => {
   return (
     <div>
+      <NavbarReactV2EN />
       <SocialEn />
+      <FooterReactEN />
     </div>
   );
 };
