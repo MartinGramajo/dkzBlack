ccd02936c40efba61819a3495f5209a65d5a9643
¨mago¨ <martingramajo08@gmail.com>
2023-08-30 18:01:34 -0300

    Agrego en audio visuales EN OTROS CASOS

--- src/en/components/produccionEN/AudioVisualesEN.jsx
+++ src/en/components/produccionEN/AudioVisualesEN.jsx
@@ -1,7 +1,30 @@
 import "./audiovisualEN.css";
 import LineaNaranja from "../../../esp/components/LineaNaranja";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Papa from "papaparse";
+import BotonContactanosEN from "../BotonContactanosEN";
+import { Link } from "react-router-dom";
+import OtrosCasosEN from "../OtrosCasosEN";
+import { Spinner } from "react-bootstrap";
 
 const AudioVisualesEN = () => {
+  const [trabajos, setTrabajos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getTrabajos = async () => {
+      setLoading(true);
+      const response = await axios.get(
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQA6RTHRyOTuR1qZesdUWlzihrNNeINy7sgWYCrDeH-vfC_K5eUsWoc66RTDtfx6AEATMBVPYafw003/pub?gid=0&single=true&output=csv"
+      );
+      const imagenes = Papa.parse(response.data, { header: true });
+      setTrabajos(imagenes.data);
+    };
+    getTrabajos();
+    setLoading(false);
+  }, []);
+
   return (
     <div className="color-negro-fondo">
       <div className="container padding-titulo">
@@ -109,35 +132,35 @@ const AudioVisualesEN = () => {
           </div>
         </div>
       </div>
-      {/* {loading ? (
+      {loading ? (
         <div className="text-white spinner py-5">
-            <Spinner
-                className="fs-1"
-                animation="border"
-                role="status"
-                variant="light"
-            ></Spinner>
+          <Spinner
+            className="fs-1"
+            animation="border"
+            role="status"
+            variant="light"
+          ></Spinner>
         </div>
-    ) : (
-        <OtrosCasos trabajos={trabajos} tipo="audiovisuales" />
-    )}
-    <div className="d-flex justify-content-center ">
+      ) : (
+        <OtrosCasosEN trabajos={trabajos} tipo="audio-visual-production" />
+      )}
+      <div className="d-flex justify-content-center ">
         <Link
-            className="text-decoration-none text-white d-none d-lg-block pb-5"
-            to="/servicios"
+          className="text-decoration-none text-white d-none d-lg-block pb-5"
+          to="/services"
         >
-            <h3 className="peso-bold">{"< < Volver"}</h3>
+          <h3 className="peso-bold">{"< < Back To"}</h3>
         </Link>
         <Link
-            className="text-decoration-none text-white d-lg-none d-block pt-5 pb-4"
-            to="/servicios"
+          className="text-decoration-none text-white d-lg-none d-block pt-5 pb-4"
+          to="/services"
         >
-            <h4 className="peso-bold">{"< < Volver"}</h4>
+          <h4 className="peso-bold">{"< < Back To"}</h4>
         </Link>
-    </div>
-    <div className="d-flex justify-content-center py-5 color-negro-a-negroclaro-fondo">
-        <BotonContactanos />
-    </div> */}
+      </div>
+      <div className="d-flex justify-content-center py-5 color-negro-a-negroclaro-fondo">
+        <BotonContactanosEN />
+      </div>
     </div>
   );
 };
