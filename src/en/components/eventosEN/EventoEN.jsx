0477942b66b859abbfc061d0fe34437d808b95c6
¨mago¨ <martingramajo08@gmail.com>
2023-09-04 00:46:02 -0300

    evento en corregido

--- src/en/components/eventosEN/EventoEN.jsx
+++ src/en/components/eventosEN/EventoEN.jsx
@@ -32,20 +32,16 @@ const EventoEn = () => {
           <h1 className="peso-bold">EVENTS</h1>
           <h4 className="py-4 peso-regular">
             In our agency, we put at your disposal a team of highly qualified
-            professionals highly qualified professionals who are capable of
-            executing and events according to the highest standards of
-            organizational quality and style. standards of organizational
-            quality and style.
+            professionals. Who are capable of executing and events according to
+            the highest standards of organizational quality and style.
           </h4>
           <h4 className="py-4 peso-regular">
             We make sure that every event we organize is a unique and
-            unforgettable experience, with a special unique and unforgettable
-            experience, with a special focus on attention to detail and customer
-            attention to detail and customer satisfaction. In addition, we offer
-            audiovisual coverage services so you can remember and share the
-            highlights of your event. share the highlights of your event, trust
-            us to take your event to the us to take your event to the next
-            level!
+            unforgettable experience, with a special focus on attention to
+            detail and customer attention to detail and customer satisfaction.
+            In addition, we offer audiovisual coverage services so you can
+            remember and share the highlights of your event. share the
+            highlights of your event.
           </h4>
           <h2 className="peso-bold color-naranja py-4">
             Organization, professionalism, quality, and attention to detail.
@@ -54,14 +50,11 @@ const EventoEn = () => {
             <h2 className="peso-bold py-4">Turnkey service:</h2>
             <h4 className="peso-regular">
               A turnkey event is an integral service in which we take care of
-              the entire process the entire process, from planning and design to
-              the execution and coordination of the to the execution and
+              the entire process, from planning and design to the execution and
               coordination of the event itself. This This means that DKZ takes
-              care of all aspects, including venue selection venue selection,
-              stage design, supplier contracting, catering, logistics catering,
-              logistics, decoration, decoration, lighting, sound, security,
-              security lighting, sound, security, among other aspects, leaving
-              you worry free.
+              care of all aspects, including venue selection, stage design,
+              supplier contracting, catering, logistics, decoration,lighting,
+              sound, security, among other aspects, leaving you worry free.
             </h4>
           </div>
         </div>
@@ -70,20 +63,16 @@ const EventoEn = () => {
           <h3 className="peso-bold py-4">EVENTS</h3>
           <h6 className="py-3 peso-regular">
             In our agency, we put at your disposal a team of highly qualified
-            professionals highly qualified professionals who are capable of
-            executing and events according to the highest standards of
-            organizational quality and style. standards of organizational
-            quality and style.
+            professionals. Who are capable of executing and events according to
+            the highest standards of organizational quality and style.
           </h6>
           <h6 className="py-3 peso-regular">
             We make sure that every event we organize is a unique and
-            unforgettable experience, with a special unique and unforgettable
-            experience, with a special focus on attention to detail and customer
-            attention to detail and customer satisfaction. In addition, we offer
-            audiovisual coverage services so you can remember and share the
-            highlights of your event. share the highlights of your event, trust
-            us to take your event to the us to take your event to the next
-            level!
+            unforgettable experience, with a special focus on attention to
+            detail and customer attention to detail and customer satisfaction.
+            In addition, we offer audiovisual coverage services so you can
+            remember and share the highlights of your event. share the
+            highlights of your event.
           </h6>
           <h5 className="peso-bold color-naranja pt-3">
             Organization, professionalism, quality, and attention to detail.
@@ -92,14 +81,11 @@ const EventoEn = () => {
             <h5 className="peso-bold py-3"> Turnkey service:</h5>
             <h6 className="peso-regular">
               A turnkey event is an integral service in which we take care of
-              the entire process the entire process, from planning and design to
-              the execution and coordination of the to the execution and
+              the entire process, from planning and design to the execution and
               coordination of the event itself. This This means that DKZ takes
-              care of all aspects, including venue selection venue selection,
-              stage design, supplier contracting, catering, logistics catering,
-              logistics, decoration, decoration, lighting, sound, security,
-              security lighting, sound, security, among other aspects, leaving
-              you worry free.
+              care of all aspects, including venue selection, stage design,
+              supplier contracting, catering, logistics, decoration,lighting,
+              sound, security, among other aspects, leaving you worry free.
             </h6>
           </div>
         </div>
