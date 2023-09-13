2fc161e85e91b15cd431c870cb5746aedf5517a8
¨mago¨ <martingramajo08@gmail.com>
2023-09-12 21:07:01 -0300

    corrijo el novedades del home

--- src/en/components/novedadesHomeEN/NovedadesHomeEN.jsx
+++ src/en/components/novedadesHomeEN/NovedadesHomeEN.jsx
@@ -16,109 +16,96 @@ import { NavLink } from "react-router-dom";
 import LineaNaranja from "../../../esp/components/LineaNaranja";
 
 export default function NovedadesHomeEN() {
-    const [trabajos, setTrabajos] = useState([]);
+  const [trabajos, setTrabajos] = useState([]);
 
-    useEffect(() => {
-        const getTrabajos = async () => {
-            const response = await axios.get(
-                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQA6RTHRyOTuR1qZesdUWlzihrNNeINy7sgWYCrDeH-vfC_K5eUsWoc66RTDtfx6AEATMBVPYafw003/pub?gid=574932737&single=true&output=csv"
-            );
-            const imagenes = Papa.parse(response.data, { header: true });
-            setTrabajos(imagenes.data);
-        };
-        getTrabajos();
-    }, []);
-    return (
-        <div className="pb-5">
-            {/* ESCRITTORIO */}
-            <div
-                id="NovedadesHome"
-                className=" padding-titulo d-none d-xl-block"
-            >
-                <div className="container">
-                    <LineaNaranja />
-                    <h2 className="color-blanco mt-5 peso-bold">NEWS</h2>
-                    <div className="d-flex flex-wrap justify-content-between pt-5 cards-novedades">
-                        {trabajos.slice(0, 3).map((trabajo, i) => (
-                            <NavLink
-                                to={"/novedades/" + trabajo.id}
-                                className=""
-                                key={i}
-                            >
-                                <div
-                                    className="fondo-novedades-home"
-                                    style={{
-                                        backgroundImage: `url(${trabajo.imagenMini})`,
-                                    }}
-                                >
-                                    <h6 className="peso-bold novedades-home-texto px-4 pt-3">
-                                        {trabajo.tituloEN}
-                                    </h6>
-                                    <h6 className="peso-regular interlineado novedades-home-texto px-4 pb-3 pt-0">
-                                        {trabajo.resumenEN}
-                                    </h6>
-                                </div>
-                            </NavLink>
-                        ))}
-                    </div>
+  useEffect(() => {
+    const getTrabajos = async () => {
+      const response = await axios.get(
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQA6RTHRyOTuR1qZesdUWlzihrNNeINy7sgWYCrDeH-vfC_K5eUsWoc66RTDtfx6AEATMBVPYafw003/pub?gid=574932737&single=true&output=csv"
+      );
+      const imagenes = Papa.parse(response.data, { header: true });
+      setTrabajos(imagenes.data);
+    };
+    getTrabajos();
+  }, []);
+  return (
+    <div className="pb-5">
+      {/* ESCRITTORIO */}
+      <div id="NovedadesHome" className=" padding-titulo d-none d-xl-block">
+        <div className="container">
+          <LineaNaranja />
+          <h2 className="color-blanco mt-5 peso-bold">NEWS</h2>
+          <div className="d-flex flex-wrap justify-content-between pt-5 cards-novedades">
+            {trabajos.slice(0, 3).map((trabajo, i) => (
+              <NavLink to={"/news/" + trabajo.id} className="" key={i}>
+                <div
+                  className="fondo-novedades-home"
+                  style={{
+                    backgroundImage: `url(${trabajo.imagenMini})`,
+                  }}
+                >
+                  <h6 className="peso-bold novedades-home-texto px-4 pt-3">
+                    {trabajo.tituloEN}
+                  </h6>
+                  <h6 className="peso-regular interlineado novedades-home-texto px-4 pb-3 pt-0">
+                    {trabajo.resumenEN}
+                  </h6>
                 </div>
-            </div>
-            {/* CELULAR */}
-            <div
-                id="NovedadesHome"
-                className="padding-titulo d-xl-none d-block "
-            >
-                <div className="container">
-                    <div className="mx-3">
-                        <LineaNaranja />
-                        <h3 className="color-blanco mt-4 peso-bold">NEWS</h3>
-                    </div>
-                </div>
-                <div className="pt-5 pb-0">
-                    <Swiper
-                        effect={"coverflow"}
-                        grabCursor={true}
-                        centeredSlides={true}
-                        initialSlide={1}
-                        navigation={false}
-                        control={true}
-                        slidesPerView={"auto"}
-                        coverflowEffect={{
-                            rotate: 0,
-                            strech: 0,
-                            depth: 100,
-                            modifier: 2.5,
-                            slideShadows: false,
-                        }}
-                        modules={[EffectCoverflow, Navigation]}
-                        spaceBetween={-20} // o margin en cada elemento
-                        className="swiper-novedades"
-                    >
-                        {trabajos.slice(0, 3).map((trabajo, i) => (
-                            <SwiperSlide key={i}>
-                                <NavLink
-                                    to={"/novedades/" + trabajo.id}
-                                    className=""
-                                >
-                                    <div
-                                        className="fondo-novedades-home"
-                                        style={{
-                                            backgroundImage: `url(${trabajo.imagenMini})`,
-                                        }}
-                                    >
-                                        <h6 className="peso-bold novedades-home-texto px-4 pt-3">
-                                            {trabajo.titulo}
-                                        </h6>
-                                        <h6 className="peso-regular interlineado novedades-home-texto px-4 pb-3 pt-0">
-                                            {trabajo.resumen}
-                                        </h6>
-                                    </div>
-                                </NavLink>
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
-                </div>
-            </div>
+              </NavLink>
+            ))}
+          </div>
         </div>
-    );
+      </div>
+      {/* CELULAR */}
+      <div id="NovedadesHome" className="padding-titulo d-xl-none d-block ">
+        <div className="container">
+          <div className="mx-3">
+            <LineaNaranja />
+            <h3 className="color-blanco mt-4 peso-bold">NEWS</h3>
+          </div>
+        </div>
+        <div className="pt-5 pb-0">
+          <Swiper
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            initialSlide={1}
+            navigation={false}
+            control={true}
+            slidesPerView={"auto"}
+            coverflowEffect={{
+              rotate: 0,
+              strech: 0,
+              depth: 100,
+              modifier: 2.5,
+              slideShadows: false,
+            }}
+            modules={[EffectCoverflow, Navigation]}
+            spaceBetween={-20} // o margin en cada elemento
+            className="swiper-novedades"
+          >
+            {trabajos.slice(0, 3).map((trabajo, i) => (
+              <SwiperSlide key={i}>
+                <NavLink to={"/news/" + trabajo.id} className="">
+                  <div
+                    className="fondo-novedades-home"
+                    style={{
+                      backgroundImage: `url(${trabajo.imagenMini})`,
+                    }}
+                  >
+                    <h6 className="peso-bold novedades-home-texto px-4 pt-3">
+                      {trabajo.titulo}
+                    </h6>
+                    <h6 className="peso-regular interlineado novedades-home-texto px-4 pb-3 pt-0">
+                      {trabajo.resumen}
+                    </h6>
+                  </div>
+                </NavLink>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
 }
