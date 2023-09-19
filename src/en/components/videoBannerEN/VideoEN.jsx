e84f27b41559808798a50d3af5f31f27db890e71
¨mago¨ <martingramajo08@gmail.com>
2023-09-18 23:37:49 -0300

    solucionado el video en ios

--- src/en/components/videoBannerEN/VideoEN.jsx
+++ src/en/components/videoBannerEN/VideoEN.jsx
@@ -18,3 +18,38 @@ const VideoEN = () => {
 };
 
 export default VideoEN;
+
+// import React, { useEffect, useRef } from "react";
+
+// const VideoEN = () => {
+//   const playerRef = useRef(null);
+//   const videoId = "XXX1bgaZJeg"; // Reemplaza con el ID de tu video de YouTube
+
+//   useEffect(() => {
+//     // Configurar el reproductor de YouTube cuando el componente se monta
+//     const player = new window.YT.Player(playerRef.current, {
+//       videoId: videoId,
+//       playerVars: {
+//         autoplay: 1,
+//         controls: 0,
+//         playsinline: 1,
+//       },
+//     });
+
+//     // Agregar un evento para reiniciar el video cuando termine
+//     player.addEventListener("onStateChange", (event) => {
+//       if (event.data === window.YT.PlayerState.ENDED) {
+//         player.seekTo(0); // Reinicia el video al finalizar
+//       }
+//     });
+
+//     // Limpieza al desmontar el componente
+//     return () => {
+//       player.destroy(); // Detiene y destruye el reproductor al desmontar el componente
+//     };
+//   }, []);
+
+//   return <div ref={playerRef}></div>;
+// };
+
+// export default VideoEN;
